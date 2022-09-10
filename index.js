be6748cfee9090834615bccf1567f7b5278e43ef
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-09-10 20:09:38 +0600

    user create successfully

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jz2aljx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const collection = client.db("test").collection("devices");
+    const userCollection = client.db("Users").collection("UserCollection");
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = await userCollection.find(query);
+      const result = await cursor.toArray(result);
+      res.send(result);
+    });
+
+    app.put("/users/:email", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          user: user,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+
+      res.send({ result, status: 200, success: true });
+    });
+
+    //
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("server Is run Successfully ");
